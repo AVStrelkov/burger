089224fa0fb89e14dfd3a14eda05c4b5a1e7ffe9
Aleksandr <34439431+AVStrelkov@users.noreply.github.com>
2018-08-01 23:17:10 +0400

    edit main.js, add accordeon

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,19 @@ function init(){
             event.target.classList.add("navigation__active");
         });
     }
+
+    var accordeon = document.querySelectorAll(".accordeon__full-name");
+    for(var i = 0; i < accordeon.length;i++){
+        
+        accordeon[i].addEventListener("click", function(event){
+            for(var i = 0; i < accordeon.length; i++){
+                accordeon[i].parentElement.lastElementChild.classList.add("accordeon__people-height");
+                accordeon[i].firstElementChild.classList.remove("accordeon__arrow-up");
+                accordeon[i].lastElementChild.classList.remove("accordeon__name-yellow");
+            }
+            event.target.previousElementSibling.classList.add("accordeon__arrow-up");
+            event.target.classList.add("accordeon__name-yellow");
+            event.path[2].lastElementChild.classList.remove("accordeon__people-height");
+        });
+    }
 }
