57e10c7db162d66bdd50317d53d614ccd10ffd02
Aleksandr <34439431+AVStrelkov@users.noreply.github.com>
2018-08-16 23:06:54 +0400

    add slider burger

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,7 @@ function init(){
     let menu = document.querySelector(".slim-menu");
     let menuContainer = document.querySelector(".hamburgermenu");
     let body = document.querySelector("body");
+    let team = document.querySelector(".accordeon__list");
 
     function toggleMenu(e){
         menuContainer.classList.toggle("is-active");
@@ -14,51 +15,97 @@ function init(){
 
     menuContainer.addEventListener("click", function(e){
         
-        if(e.target.className === "sidebar__link"){
-            menuContainer.classList.toggle("is-active");
-            menu.classList.toggle("rotate");
-            body.classList.toggle("body-active");
-        }
+        if(e.target.className === "sidebar__link") toggleMenu();
     });
 
+    team.addEventListener("click", teamAccordeon);
 
+    function teamAccordeon(e){
+        for(let i=0; i < team.children.length; i++){
+            let peopleHeight = team.children[i].lastElementChild;
+            peopleHeight.classList.add("accordeon__people-height");
+        }
+        let arrow = e.target.previousElementSibling;
+        let people = e.target.parentElement.nextElementSibling;
+        // console.log(people);
+        arrow.classList.toggle("accordeon__arrow-up");
+        people.classList.toggle("accordeon__people-height");
+    }
+
+    // slider
+
+    let wrapper = document.querySelector(".wrapper-sleder");
+    let content = document.querySelector(".content");
+    let btnLeft = document.querySelector(".slide-arrow__left");
+    let btnRight = document.querySelector(".slide-arrow__right");
+    let slide = document.querySelectorAll(".slide");
+    let computed = content.clientWidth;
+    let right = 0;
+    slide[0].style.width = computed + "px";
+    slide[1].style.width = computed + "px";
+    slide[2].style.width = computed + "px";
+    wrapper.style.width = computed * slide.length + "px";
+    // console.dir(content.clientWidth);
+
+    
+    
+    btnLeft.addEventListener("click", function(e){
+        e.preventDefault();
+        let wrapperRight = parseInt(wrapper.style.right);
+        let wrapperWidth = parseInt(wrapper.style.width);
+        if (wrapperRight <= -wrapperWidth + computed){
+            wrapper.style.right = 0 + "px";
+            let right = wrapper.style.right/computed;
+        } else{
+            wrapper.style.right = wrapperRight - computed +"px";
+        }
+    });
+    btnRight.addEventListener("click", function(e){
+        e.preventDefault();
+        let wrapperRight = parseInt(wrapper.style.right);
+        let wrapperWidth = parseInt(wrapper.style.width);
+        if (wrapperRight >= 0){
+            wrapper.style.right = -wrapperWidth + computed + "px";
+            let right = wrapper.style.right/computed;
+        } else{
+            wrapper.style.right = wrapperRight + computed +"px";
+        }
+    });
+    
+    
+    window.addEventListener('resize', function(){
+    // let wrapperRight = document.querySelector(".wrapper-sleder");
+    let content = document.querySelector(".content");
+    let slide = document.querySelectorAll(".slide");
+    let computed = content.clientWidth;
+    slide[0].style.width = computed + "px";
+    slide[1].style.width = computed + "px";
+    slide[2].style.width = computed + "px";
+    wrapper.style.width = computed * slide.length + "px";
+    let wrapperRight = parseInt(wrapper.style.right);
+    let right = wrapperRight/computed;
+    wrapper.style.right=right*computed+"px";
+    // wrapper.style.right = computed * right;
+    console.log(right);
+});
+    
 }
 
 
-
-// let humburger = (function init(){
-
-//     let _toggleMenu = function(e){
-//         button.classList.toggle("is-active");
-//         menu.classList.toggle("is-active");
-//         body.classList.toggle("locked");
-//     }
-
-//     let daaListeners = function(){
-//         button.addEventListener("click", _toggleMenu);
-//         menu.addEventListener("click", function(e){
-//             console.log(e.target.className)
-//         })
-//     }
-
-// })({
-//     button: ""
-// })
-
 //     var nav = document.querySelectorAll(".navigation__link");
 //     for(var i =0; i < nav.length; i++){
-//         nav[i].addEventListener("click", function(event){
-//             for(var i=0; i < nav.length; i++){
-//                 nav[i].classList.remove("navigation__active");
-//             }
-//             event.target.classList.add("navigation__active");
-//         });
-//     }
-
-//     var accordeon = document.querySelectorAll(".accordeon__full-name");
+    //         nav[i].addEventListener("click", function(event){
+        //             for(var i=0; i < nav.length; i++){
+            //                 nav[i].classList.remove("navigation__active");
+            //             }
+            //             event.target.classList.add("navigation__active");
+            //         });
+            //     }
+            
+            //     var accordeon = document.querySelectorAll(".accordeon__full-name");
 //     for(var i = 0; i < accordeon.length;i++){
-        
-//         accordeon[i].addEventListener("click", function(event){
+    
+    //         accordeon[i].addEventListener("click", function(event){
 //             for(var i = 0; i < accordeon.length; i++){
 //                 accordeon[i].parentElement.lastElementChild.classList.add("accordeon__people-height");
 //                 accordeon[i].firstElementChild.classList.remove("accordeon__arrow-up");
